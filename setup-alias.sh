817f6617b37826719e73891493d0ae25cc022223
MoeBrowne <oliver.browne@uplyme.com>
2016-01-31 02:03:59 +0000

    Added missing alias command and quotes

--- setup-alias.sh
+++ setup-alias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat << 'ALIAS' >> ~/.bash_aliases
-unset GIT_SSH_COMMAND; git config -l | grep -q ssh.keypath; test $? -eq 0 && export GIT_SSH_COMMAND=\"ssh -i $HOME/.ssh/your-key\"; git
+alias git='unset GIT_SSH_COMMAND; git config -l | grep -q ssh.keypath; test $? -eq 0 && export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/your-key"; git'
 ALIAS
