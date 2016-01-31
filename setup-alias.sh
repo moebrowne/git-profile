367b3277a6c2a6052918b6acd30762cf458d21a1
MoeBrowne <oliver.browne@uplyme.com>
2016-01-31 02:02:43 +0000

    Converted the echo to the more readable here document

--- setup-alias.sh
+++ setup-alias.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo "unset GIT_SSH_COMMAND; git config -l | grep -q ssh.keypath; test $? -eq 0 && export GIT_SSH_COMMAND=\"ssh -i $HOME/.ssh/your-key\"; git" > ~/.bash_aliases
+cat << 'ALIAS' >> ~/.bash_aliases
+unset GIT_SSH_COMMAND; git config -l | grep -q ssh.keypath; test $? -eq 0 && export GIT_SSH_COMMAND=\"ssh -i $HOME/.ssh/your-key\"; git
+ALIAS
