de418cd5b656296b861dfafe77fec451b1c17262
MoeBrowne <oliver.browne@uplyme.com>
2016-01-31 02:17:44 +0000

    Updated the alias to use the in built git config command to check and get the value

--- setup-alias.sh
+++ setup-alias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat << 'ALIAS' >> ~/.bash_aliases
-alias git='unset GIT_SSH_COMMAND; git config -l | grep -q ssh.keypath; test $? -eq 0 && export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/your-key"; git'
+alias git='unset GIT_SSH_COMMAND; git config SSH.keypath &> /dev/null; test $? -eq 0 && export GIT_SSH_COMMAND="ssh -i $(/usr/bin/git config SSH.keypath)"; git'
 ALIAS
