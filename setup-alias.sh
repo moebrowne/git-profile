7383c5f984cdf40a1839e436a28fafb37d44773a
MoeBrowne <oliver.browne@uplyme.com>
2016-01-31 02:38:56 +0000

    A little clean up

--- setup-alias.sh
+++ setup-alias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cat << 'ALIAS' >> ~/.bash_aliases
-alias git='unset GIT_SSH_COMMAND; sshkey="$(/usr/bin/git config SSH.keypath)"; [ "$sshkey" != "" ] && export GIT_SSH_COMMAND="ssh -i $sshkey"; git'
+alias git='unset GIT_SSH_COMMAND; sshkey="$(/usr/bin/git config SSH.keypath)"; [ "$sshkey" != "" ] && export GIT_SSH_COMMAND="ssh -i $sshkey"; unset sshkey; git'
 ALIAS
